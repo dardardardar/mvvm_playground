733a8dbf343f38a295895512ce004600c9bdfa77
Dar <fauzi@infotech.co.id>
2024-05-21 14:11:10 +0700

    aa

--- lib/features/pages/main_menu_page.dart
+++ lib/features/pages/main_menu_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_playground/const/theme.dart';
+import 'package:mvvm_playground/widgets/buttons.dart';
 
 class MainMenuPage extends StatelessWidget {
   const MainMenuPage({super.key});
@@ -11,9 +12,34 @@ class MainMenuPage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          color: primaryColor,
           child: Column(
-            children: [Text('Go Harvest')],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Go Harvest'.toUpperCase(),
+                  style: textHeading.copyWith(fontSize: 24),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              flatButton(
+                  context: context,
+                  title: 'Harvest',
+                  backgroundColor: primaryColor,
+                  icon: Icons.shopping_cart_rounded),
+              SizedBox(
+                height: 8,
+              ),
+              flatButton(
+                  context: context,
+                  onTap: () {},
+                  title: 'History',
+                  backgroundColor: primaryColor,
+                  icon: Icons.timer_outlined)
+            ],
           ),
         ),
       ),
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mvvm_playground/const/theme.dart';
 
 Widget primaryButton({Function()? onPressed, required String title}) {
@@ -6,5 +7,70 @@ Widget primaryButton({Function()? onPressed, required String title}) {
     onPressed: onPressed,
     color: primaryColor,
     child: Text(title),
+  );
+}
+
+Widget bigButton(
+    {required BuildContext context,
+    Function()? onTap,
+    required String title,
+    Color? backgroundColor,
+    Color? color,
+    required IconData icon}) {
+  return InkWell(
+    onTap: onTap,
+    child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: backgroundColor),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              color: color,
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              title,
+            ),
+          ],
+        )),
+  );
+}
+
+Widget flatButton(
+    {required BuildContext context,
+    Function()? onTap,
+    required String title,
+    Color? backgroundColor,
+    Color? color,
+    required IconData icon}) {
+  return InkWell(
+    onTap: onTap,
+    child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: backgroundColor),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Icon(
+                icon,
+                color: color,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: Text(
+                title,
+                style: textHeading2Alt,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        )),
   );
 }
