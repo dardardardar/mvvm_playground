386184bc42d8383bda09605fe30083b02bccc752
Dar <heidarneil@kelasduniaekasakti.co.id>
2024-07-02 16:55:52 +0700

    z

--- lib/features/pages/main_menu_page.dart
+++ lib/features/pages/main_menu_page.dart
@@ -29,7 +29,13 @@ class _MainMenuPageState extends State<MainMenuPage> {
   @override
   void initState() {
     super.initState();
+    initSync();
     checkNav = true;
+  }
+
+  void initSync() {
+    Future.delayed(Duration(seconds: 2));
+    context.read<MapsCubit>().installation('online');
   }
 
   String buttontext = 'Sync';
@@ -115,51 +121,45 @@ class _MainMenuPageState extends State<MainMenuPage> {
                           ),
                         ),
                         const SizedBox(height: 100),
-                        Visibility(
-                          visible: (buttontext != 'Loading..'),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: flatButton(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const FlutterMapPage(
-                                      isHistory: false,
-                                      title: 'Harvest',
-                                    ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: flatButton(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const FlutterMapPage(
+                                    isHistory: false,
+                                    title: 'Harvest',
                                   ),
-                                );
-                              },
-                              context: context,
-                              title: 'Harvest',
-                              backgroundColor: primaryColor,
-                              icon: Icons.shopping_cart_rounded,
-                              color: Colors.white,
-                            ),
+                                ),
+                              );
+                            },
+                            context: context,
+                            title: 'Harvest',
+                            backgroundColor: primaryColor,
+                            icon: Icons.shopping_cart_rounded,
+                            color: Colors.white,
                           ),
                         ),
-                        Visibility(
-                          visible: (buttontext != 'Loading..'),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: flatButton(
-                              context: context,
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const HasilPanenPage(
-                                      title: 'Hasil Panen',
-                                    ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: flatButton(
+                            context: context,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HasilPanenPage(
+                                    title: 'Hasil Panen',
                                   ),
-                                );
-                              },
-                              title: 'Hasil Panen',
-                              backgroundColor: primaryColor,
-                              icon: Icons.timer_outlined,
-                              color: Colors.white,
-                            ),
+                                ),
+                              );
+                            },
+                            title: 'Hasil Panen',
+                            backgroundColor: primaryColor,
+                            icon: Icons.timer_outlined,
+                            color: Colors.white,
                           ),
                         ),
                         Visibility(
@@ -180,29 +180,26 @@ class _MainMenuPageState extends State<MainMenuPage> {
                             ),
                           ),
                         ),
-                        Visibility(
-                          visible: (buttontext != 'Loading..'),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: flatButton(
-                              context: context,
-                              onTap: () {
-                                context.read<AuthCubit>().logout();
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const LoginPage(),
-                                  ),
-                                );
-                                showSnackbar(context,
-                                    message: 'Berhasil Logout',
-                                    status: Status.Success);
-                              },
-                              title: 'Logout',
-                              backgroundColor: primaryColor,
-                              icon: Icons.timer_outlined,
-                              color: Colors.white,
-                            ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: flatButton(
+                            context: context,
+                            onTap: () {
+                              context.read<AuthCubit>().logout();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginPage(),
+                                ),
+                              );
+                              showSnackbar(context,
+                                  message: 'Berhasil Logout',
+                                  status: Status.Success);
+                            },
+                            title: 'Logout',
+                            backgroundColor: primaryColor,
+                            icon: Icons.timer_outlined,
+                            color: Colors.white,
                           ),
                         ),
                         const SizedBox(height: 100),
